28e152a1a13350a0fe5f7c576ce85a4f0d80cddc
Shailee Patel <shaileepatel2000@gmail.com>
2021-07-26 23:12:10 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Lab5TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.MenuEdit,
+            this.MenuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // MenuFile
+            // 
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuNew,
+            this.toolStripMenuOpen,
+            this.toolStripMenuSave,
+            this.toolStripMenuSaveAs,
+            this.toolStripMenuClose,
+            this.toolStripMenuExit});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(46, 24);
+            this.MenuFile.Text = "&File";
+            // 
+            // toolStripMenuNew
+            // 
+            this.toolStripMenuNew.Name = "toolStripMenuNew";
+            this.toolStripMenuNew.ShortcutKeyDisplayString = "";
+            this.toolStripMenuNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.toolStripMenuNew.Size = new System.Drawing.Size(233, 26);
+            this.toolStripMenuNew.Text = "New";
+            // 
+            // toolStripMenuOpen
+            // 
+            this.toolStripMenuOpen.Name = "toolStripMenuOpen";
+            this.toolStripMenuOpen.ShortcutKeyDisplayString = "";
+            this.toolStripMenuOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.toolStripMenuOpen.Size = new System.Drawing.Size(233, 26);
+            this.toolStripMenuOpen.Text = "Open";
+            // 
+            // toolStripMenuSave
+            // 
+            this.toolStripMenuSave.Name = "toolStripMenuSave";
+            this.toolStripMenuSave.ShortcutKeyDisplayString = "";
+            this.toolStripMenuSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.toolStripMenuSave.Size = new System.Drawing.Size(233, 26);
+            this.toolStripMenuSave.Text = "Save";
+            // 
+            // toolStripMenuSaveAs
+            // 
+            this.toolStripMenuSaveAs.Name = "toolStripMenuSaveAs";
+            this.toolStripMenuSaveAs.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.toolStripMenuSaveAs.Size = new System.Drawing.Size(233, 26);
+            this.toolStripMenuSaveAs.Text = "Save As";
+            // 
+            // toolStripMenuClose
+            // 
+            this.toolStripMenuClose.Name = "toolStripMenuClose";
+            this.toolStripMenuClose.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.C)));
+            this.toolStripMenuClose.Size = new System.Drawing.Size(233, 26);
+            this.toolStripMenuClose.Text = "Close";
+            // 
+            // toolStripMenuExit
+            // 
+            this.toolStripMenuExit.Name = "toolStripMenuExit";
+            this.toolStripMenuExit.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.X)));
+            this.toolStripMenuExit.Size = new System.Drawing.Size(233, 26);
+            this.toolStripMenuExit.Text = "Exit";
+            // 
+            // MenuEdit
+            // 
+            this.MenuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuCopy,
+            this.toolStripMenuCut,
+            this.toolStripMenuPaste});
+            this.MenuEdit.Name = "MenuEdit";
+            this.MenuEdit.Size = new System.Drawing.Size(49, 24);
+            this.MenuEdit.Text = "&Edit";
+            // 
+            // toolStripMenuCopy
+            // 
+            this.toolStripMenuCopy.Name = "toolStripMenuCopy";
+            this.toolStripMenuCopy.ShortcutKeyDisplayString = "";
+            this.toolStripMenuCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.toolStripMenuCopy.Size = new System.Drawing.Size(177, 26);
+            this.toolStripMenuCopy.Text = "&Copy";
+            // 
+            // toolStripMenuCut
+            // 
+            this.toolStripMenuCut.Name = "toolStripMenuCut";
+            this.toolStripMenuCut.ShortcutKeyDisplayString = "";
+            this.toolStripMenuCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.toolStripMenuCut.Size = new System.Drawing.Size(177, 26);
+            this.toolStripMenuCut.Text = "C&ut";
+            // 
+            // toolStripMenuPaste
+            // 
+            this.toolStripMenuPaste.Name = "toolStripMenuPaste";
+            this.toolStripMenuPaste.ShortcutKeyDisplayString = "";
+            this.toolStripMenuPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.toolStripMenuPaste.Size = new System.Drawing.Size(177, 26);
+            this.toolStripMenuPaste.Text = "Paste";
+            // 
+            // MenuHelp
+            // 
+            this.MenuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuAbout});
+            this.MenuHelp.Name = "MenuHelp";
+            this.MenuHelp.Size = new System.Drawing.Size(55, 24);
+            this.MenuHelp.Text = "Help";
+            // 
+            // toolStripMenuAbout
+            // 
+            this.toolStripMenuAbout.Name = "toolStripMenuAbout";
+            this.toolStripMenuAbout.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.toolStripMenuAbout.Size = new System.Drawing.Size(185, 26);
+            this.toolStripMenuAbout.Text = "About";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text Editor Select a File to Open";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuNew;
+        private System.Windows.Forms.ToolStripMenuItem MenuEdit;
+        private System.Windows.Forms.ToolStripMenuItem MenuHelp;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuSave;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuClose;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuCopy;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuCut;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuPaste;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuAbout;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿// Shailee Patel (100800440)
+// July 24, 2021
+// Description - The purpose of this lab is to create a text editor.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab5TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        private void toolStripMenuOpenClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath);
+            
+
+        }
+        private void toolStripMenuSaveClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    myStream.Close();
+                }
+            }
+
+
+        }
+        private void toolStripMenuSaveAsClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+            toolStripMenuSave.Text();
+         
+        }
+        private void toolStripMenuNewClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        private void toolStripMenuExitClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (statusChange.Text == "True")
+            {
+                if (MessageBox.Show("You have unsaved changes, Are you sure you want to exit?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("Are you sure you want to exit?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+
+
+        }
+
+    }
+}
